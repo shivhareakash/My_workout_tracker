109fa3ffc2c73df3069726833a5d3568c8360386
Akash Shivhare <shivhare.akash@hotmail.com>
2021-01-07 00:07:55 -0500

    First Working Version, pending styling, url addition, testsing

--- app_workout_tracker/urls.py
+++ app_workout_tracker/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+
 app_name= 'awt'
 urlpatterns =[
     ##**************PUBLIC VIEW***********##
@@ -15,18 +16,19 @@ urlpatterns =[
     # Secure topic list
     path('secure_list/', views.secure_topics_list.as_view(), name='secure_topics_list'),
     # Secure sub_topic list
-    path('secure_list/<int:topic_id>/subtopic_list/<str:subtopic_name>', views.secure_subtopic_list.as_view(), name='secure_subtopic_list'),
+    path('secure_list/<int:topic_id>/subtopic_list/<str:topic_name>/<str:subtopic_name>/', views.secure_subtopic_list.as_view(), name='secure_subtopic_list'),
     # Secure sub_topic detail
-    path('secure_list/<int:pk>/<str:subtopic_name>',views.secure_subtopic_detail, name='secure_subtopic_detail'),
+    path('secure_list/<str:topic_id>/<str:subtopic_name>/<int:subtopic_id>/',views.secure_subtopic_detail, name='secure_subtopic_detail'),
             ##(The DetailView generic view expects the primary key value captured from the URL to be called "pk")##
-
     # Secure topic add
     path('add_topic/', views.add_topic, name='add_topic'),
     # Secure topic delete
     path('delete_topic/<int:topic_id>/', views.delete_topic, name='delete_topic'),
     # Secure subtopic add
-    path('add_subtopic/<int:topic_id>/', views.add_subtopic, name='add_subtopic')
+    path('add_subtopic/<int:topic_id>/<str:subtopic>', views.add_subtopic, name='add_subtopic'),
     # Secure subtopic edit
+    path('edit_subtopic/<int:subtopic_id>/<str:subtopic_name>', views.edit_subtopic, name='edit_subtopic'),
     # Secure subtopic delete
+    path('delete_entry/<str:subtopic_name>/<int:subtopic_id>', views.delete_subtopic, name='delete_subtopic')
 
 ]
--- app_workout_tracker/views.py
+++ app_workout_tracker/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 from .forms import TopicForm, GoalForm, MistakeForm, ProgressForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import Http404
 
 # Create your views here.
 
@@ -77,18 +78,20 @@ class secure_subtopic_list(LoginRequiredMixin, generic.ListView):
     context_object_name = 'called_submodel'
 
     def get_queryset(self, **kwargs):
-        self.subtopic_name = self.kwargs.get('subtopic_name')
-        self.topic_id = self.kwargs.get('topic_id')
-        secure_topic = get_object_or_404(Topic.objects.filter(owner=self.request.user), id=self.topic_id)
+        subtopic_name = self.kwargs.get('subtopic_name')
+        topic_id = self.kwargs.get('topic_id')
+        #Passing topic_name for display purpose on the template only
+        topic_name=self.kwargs.get('topic_name')
+        secure_topic = get_object_or_404(Topic.objects.filter(owner=self.request.user), id=topic_id)
 
-        if self.subtopic_name=='goals_list':
-            called_submodel= {self.subtopic_name:secure_topic.goal_set.all()}
+        if subtopic_name=='goals_list':
+            called_submodel= [subtopic_name, secure_topic.goal_set.all(), topic_name, topic_id]
 
-        elif self.subtopic_name=='progress_list':
-            called_submodel= {self.subtopic_name:secure_topic.progress_set.all()}
+        elif subtopic_name=='progress_list':
+            called_submodel= [subtopic_name, secure_topic.progress_set.all(), topic_name, topic_id]
 
-        elif self.subtopic_name=='mistakes_list':
-            called_submodel= {self.subtopic_name:secure_topic.mistake_set.all()}
+        elif subtopic_name=='mistakes_list':
+            called_submodel= [subtopic_name, secure_topic.mistake_set.all(), topic_name, topic_id]
 
         return called_submodel
 
@@ -96,8 +99,9 @@ class secure_subtopic_list(LoginRequiredMixin, generic.ListView):
 def secure_subtopic_detail(request, **kwargs):
     """View secure subtopic detail"""
 
-    subtopic_id= kwargs.get('pk')
+    subtopic_id= kwargs.get('subtopic_id')
     subtopic_name=kwargs.get('subtopic_name')
+    topic_id=kwargs.get('topic_id')
 
     if subtopic_name=='goals_list':
         filtered_submodel = Goal.objects.filter(topic__owner=request.user)
@@ -111,7 +115,7 @@ def secure_subtopic_detail(request, **kwargs):
         filtered_submodel = Mistake.objects.filter(topic__owner=request.user)
         detail = get_object_or_404(filtered_submodel, id=subtopic_id)
 
-    context = {'detail': detail}
+    context = {'detail': detail, 'subtopic_name':subtopic_name, 'topic_id':topic_id}
 
     return render(request, 'workout_tracker/secure_entry_detail.html', context)
 
@@ -139,32 +143,113 @@ def delete_topic(request, topic_id):
     topic.delete()
     return redirect('awt:secure_topics_list')
 
+
 @login_required
-def add_subtopic(request, topic_id):
-    '''Editing topic entry'''
+def add_subtopic(request, topic_id, subtopic):
+    '''Editing topic entry and redirecting to subtopic_list page'''
 
     topic = Topic.objects.get(id=topic_id)
-    if topic.owner==request.user:
-        if request.method!='POST':
-            subtopic_form = GoalForm()
+    if topic.owner == request.user:
+
+        if request.method != 'POST':
+            # No data, create a blank form Check the subtopic_name, and generate the relevant form##
+            if subtopic == 'goals_list':
+                subtopic_form = GoalForm()
+            elif subtopic == 'progress_list':
+                subtopic_form = ProgressForm()
+            elif subtopic == 'mistakes_list':
+                subtopic_form = MistakeForm()
+
         else:
-            subtopic_form = GoalForm(request.POST)
+            # Post data submitted, process the form on the relevant form
+            if subtopic == 'goals_list':
+                subtopic_form = GoalForm(request.POST)
+            elif subtopic == 'progress_list':
+                subtopic_form = ProgressForm(request.POST)
+            elif subtopic == 'mistakes_list':
+                subtopic_form = MistakeForm(request.POST)
+
             if subtopic_form.is_valid():
                 new_subtopic_form = subtopic_form.save(commit=False)
-                new_subtopic_form.topic=topic
+                new_subtopic_form.topic = topic
                 new_subtopic_form.save()
-                return redirect('awt:secure_subtopic_list')
-        context = {'subtopic_form':subtopic_form}
-        return render(request, 'workout_tracker/add_subtpoic.html', context)
 
+                return redirect('awt:secure_subtopic_list', topic_name=topic, topic_id=topic_id, subtopic_name=subtopic)
 
+        context = {'subtopic_form': subtopic_form, 'topic': topic, 'subtopic': subtopic}
+        # Passing topic:topic here to generate topic.id required for submitting the form.
+        return render(request, 'workout_tracker/add_subtopic.html', context)
+
+    else:
+        # User is not authorised
+        raise Http404
+
+@login_required
+def edit_subtopic(request, subtopic_id, subtopic_name):
+    '''Editing topic entry and redirecting to subtopic_detail page: which needs subtopic_id, subtopic_name, topic_id kwarg'''
+    if request.method != 'POST':
+        if subtopic_name == 'goals_list':
+            subtopic = Goal.objects.get(id=subtopic_id)
+            if subtopic.topic.owner == request.user:
+                subtopic_form = GoalForm(instance=subtopic)
+            else:
+                raise Http404
+
+        elif subtopic_name == 'progress_list':
+            subtopic = Progress.objects.get(id=subtopic_id)
+            if subtopic.topic.owner == request.user:
+                subtopic_form = ProgressForm(instance=subtopic)
+            else:
+                raise Http404
+
+        elif subtopic_name == 'mistakes_list':
+            subtopic = Mistake.objects.get(id=subtopic_id)
+            if subtopic.topic.owner == request.user:
+                subtopic_form = MistakeForm(instance=subtopic)
+            else:
+                raise Http404
+    else:
+        if subtopic_name == 'goals_list':
+            subtopic = Goal.objects.get(id=subtopic_id)
+            if subtopic.topic.owner == request.user:
+                subtopic_form = GoalForm(instance=subtopic, data=request.POST)
+        elif subtopic_name == 'progress_list':
+            subtopic = Progress.objects.get(id=subtopic_id)
+            if subtopic.topic.owner == request.user:
+                subtopic_form = ProgressForm(instance=subtopic, data=request.POST)
+            else:
+                raise Http404
+
+        elif subtopic_name == 'mistakes_list':
+            subtopic = Mistake.objects.get(id=subtopic_id)
+            if subtopic.topic.owner == request.user:
+                subtopic_form = MistakeForm(instance=subtopic, data=request.POST)
+            else:
+                raise Http404
+
+        if subtopic_form.is_valid():
+            subtopic_form.save()
+
+            return redirect('awt:secure_subtopic_detail', topic_id=subtopic.topic.id, subtopic_name=subtopic_name,subtopic_id=subtopic_id)
+
+    context = {'subtopic_form':subtopic_form, 'subtopic_name':subtopic_name, 'subtopic_id':subtopic_id}
+    return render(request, 'workout_tracker/edit_subtopic.html', context)
 
 
 @login_required
-def edit_subtopic(request):
-    '''Editing topic entry'''
-    pass
-@login_required
-def delete_subtopic(request):
-    '''Editing topic entry'''
-    pass
+def delete_subtopic(request, subtopic_id, subtopic_name):
+    '''Deleting topic entry'''
+    if subtopic_name=='goals_list':
+        subtopic = Goal.objects.get(id=subtopic_id)
+    elif subtopic_name=='progress_list':
+        subtopic = Progress.objects.get(id=subtopic_id)
+    elif subtopic_name=='mistakes_list':
+        subtopic = Mistake.objects.get(id=subtopic_id)
+
+    if subtopic.topic.owner == request.user:
+        subtopic.delete()
+
+    return redirect('awt:secure_subtopic_list', topic_name=subtopic.topic, topic_id=subtopic.topic.id, subtopic_name=subtopic_name)
+
+
+
