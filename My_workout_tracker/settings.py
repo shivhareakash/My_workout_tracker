45fef661213af165293e4f7c76c068c35eeda96d
Akash Shivhare <shivhare.akash@hotmail.com>
2021-01-10 09:38:41 -0500

    renaming static to static_files in base dir

--- My_workout_tracker/settings.py
+++ My_workout_tracker/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static_files')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 #Bootstrap5 settings:
